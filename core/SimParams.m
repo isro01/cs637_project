17d7de7b65a8133442a2ac1696e1fabf0b80d7f7
Ishir Roongta <ishir@iitk.ac.in>
2021-10-11 15:55:24 +0530

    Add sim params from appendix C

---
+++ core/SimParams.m
@@ -0,0 +1,40 @@
+classdef SimParams
+    properties
+        v_max = 10;
+        v_min = 0;
+        a_max = 3.5; 
+        a_min = -3.5;
+        u_j_max = 4;
+        u_j_min = -4;
+        del_max = 1;
+        del_min = -1;
+        omega_max = 0.5;
+        omega_min = -0.5;
+        u_s_max = 2;
+        u_s_min = -2;
+        w = 1.8;
+        l = 4;
+        l_f = 2;
+        l_r = 2;
+        d_1 = 1;
+        eta_1 = 0.067
+        v_max_s = 7;
+        v_min_s = 3;
+        a_max_s = 2.5;
+        a_lat_m = 2.5;
+        a_lat_s = 1.75;
+        d_7 = 0.3;eta_7 = 0.13;
+        d_8_l = 0.5;
+        d_8_r = 0.5;
+        d_8_f = 1;
+        eta_8_r = 0.036;
+        eta_8_l = 0.036;
+        eta_8_f = 2;
+        v_d = 4;
+        beta = 2;
+    end
+end
+
+        
+        
+        
